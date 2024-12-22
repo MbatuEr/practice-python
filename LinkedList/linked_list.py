5f9751685f2ac68abfa37cce6f906577aecfe910
MbatuEr <eryilmazmehmetbatuhan@gmail.com>
2024-12-22 22:24:23 +0300

    some interview questions about linkedlist are solved

---
+++ LinkedList/linked_list.py
@@ -0,0 +1,238 @@
+from node import Node
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def _del_(self):
+        current = self.head
+        while current:
+            next_node = current.next
+            del current
+            current = next_node
+    
+    # Append a new node to the end of the list
+    def append(self,data):
+        if not self.head:
+            self.head = Node(data)
+            return
+        
+        current = self.head
+        while current.next:
+            current = current.next
+
+        current.next = Node(data)
+
+    # Print the list
+    def print_list(self):
+        current = self.head
+        while current:
+            print(f"{current.data} -> ", end="")
+            current = current.next
+        print("None")
+
+    # Find kth to last element
+    def find_kth_to_last(self, k):
+        if not self.head:
+            raise ValueError("List is empty")
+        
+        first = self.head
+        second = self.head
+
+        for i in range(k):
+            if not first:
+                raise ValueError("k is larger than the list size")
+            first = first.next
+        
+        while first:
+            first = first.next
+            second =second.next
+        
+        return second.data
+
+    # Delete middle node
+    def delete_middle_node(self):
+        if not self.head or not self.head.next:
+            print("List is too short to have a middle node")
+            return
+        slow = self.head
+        fast = self.head
+        prev = None
+
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        
+        if prev:
+            prev.next = slow.next
+            del slow
+
+    # Partition
+    def partition(self,x):
+        if not self.head and not self.head.next:
+            return
+        
+        less_head = Node(0)
+        greater_head = Node(0)
+        less = less_head
+        greater = greater_head
+        current = self.head
+
+        while current:
+            if current.data < x:
+                less.next = current
+                less = less.next
+            else:
+                greater.next = current
+                greater = greater.next
+            current = current.next
+        
+        greater.next = None
+        less.next = greater_head.next
+        self.head = less_head.next
+        del less_head
+        del greater_head
+
+    # Reverse the list
+    def reverse(self, head):
+        prev = None
+        current = head
+
+        while current:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+
+        return prev
+
+    # Add numbers in reverse order
+    def add_reverse_order(self, l1, l2):
+        result = LinkedList()
+        carry = 0
+
+        while l1 or l2 or carry:
+            sum = carry
+            if l1:
+                sum += l1.data
+                l1 = l1.next
+            if l2:
+                sum += l2.data
+                l2 = l2.next
+            result.append(sum % 10)
+            carry = sum // 10
+
+        result.head = self.reverse(result.head)
+        return result
+
+    # Add numbers in forward order
+    def add_numbers_forward(self, l1, l2):
+        l1 = self.reverse(l1)
+        l2 = self.reverse(l2)
+
+        reversed_result = self.add_reverse_order(l1, l2)
+        reversed_result.head = self.reverse(reversed_result.head)
+
+        return reversed_result
+        
+    # Remove duplicates
+    def remove_duplicates(self):
+        if not self.head:
+            return
+        
+        seen = set()
+        current = self.head
+        prev = None
+
+        while current:
+            if current.data in seen:
+                prev.next = current.next
+                del current
+            else:
+                seen.add(current.data)
+                prev = current
+            current = prev.next 
+
+    # check if the list is a palindrome
+    def is_palindrome(self):
+        if not self.head or not self.head.next:
+            return True # Empty or single element list is a palindrome
+
+        slow = self.head
+        fast = self.head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        second_half = self.reverse(slow)
+        first_half = self.head
+
+        is_palindrome = True
+
+        while second_half:
+            if first_half.data != second_half.data:
+                is_palindrome = False
+                break
+            first_half = first_half.next
+            second_half = second_half.next
+
+        return is_palindrome    
+
+    # to get the length of the list  
+    def get_length(self, head):
+        length = 0
+        current = head
+        while current:
+            length += 1
+            current = current.next
+        return length
+
+    # Find the intersection of two lists
+    def first_intersection(self, head1, head2):
+        if not head1 or not head2:
+            return None
+
+        len1 = self.get_length(head1)
+        len2 = self.get_length(head2)
+
+        longer = head1 if len1 > len2 else head2
+        shorter = head2 if len1 > len2 else head1
+
+        length_diff = abs(len1 - len2)
+
+        for _ in range(length_diff):
+            longer = longer.next
+
+        while longer and shorter:
+            if longer == shorter:
+                return longer
+            longer = longer.next
+            shorter = shorter.next
+
+        return None
+    
+    # Detect loop in the list if any
+    def detect_loop(self, head):
+        if not head or not head.next:
+            return None
+        
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+            
+        if not fast or not fast.next:           # If no loop was found, return None
+            return None
+
+        slow = head
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next   
+        
+        return slow
---
+++ LinkedList/main.py
@@ -0,0 +1,127 @@
+from linked_list import LinkedList
+from node import Node
+
+if __name__ == "__main__":
+    list = LinkedList()
+
+    list.append(3)
+    list.append(5)
+    list.append(8)
+    list.append(5)
+    list.append(8)
+    list.append(5)
+    list.append(3)
+
+    # Find kth to last
+    print("\nOriginal list:")
+    list.print_list()
+
+    k = int(input("\nEnter k to find kth to last element: "))
+    try:
+        result = list.find_kth_to_last(k)
+        print(f"\n{k}th to last element is: {result}")
+    except ValueError as e:
+        print(f"Error: {e}")
+    print("-------------------------------------------------")
+
+    # Delete middle node
+    print("Deleting middle node...")
+    list.delete_middle_node()
+    print("\nList after deleting middle node:")
+    list.print_list()
+    print("-------------------------------------------------")    
+
+    # Partition
+    print("\nOriginal list:")
+    list.print_list()
+    partition = int(input("\nEnter partition value: "))
+    list.partition(partition)
+    print("\nList after partition:")
+    list.print_list()
+    print("-------------------------------------------------")
+
+    # Add numbers in reverse order
+    list1 = LinkedList()
+    list1.append(3)
+    list1.append(4)
+    list1.append(3)
+
+    list2 = LinkedList()
+    list2.append(4)
+    list2.append(6)
+    list2.append(5)
+    
+    print("First lists:")
+    list1.print_list()
+    print("Second list:")
+    list2.print_list()
+    result = list.add_reverse_order(list1.head, list2.head)
+    print("The sum of the two lists in reverse order is:")
+    result.print_list()
+    print("-------------------------------------------------")
+
+    # Add numbers in forward order
+    result_forward = list.add_numbers_forward(list1.head, list2.head)
+    print("The sum of the two lists in forward order is:")
+    result_forward.print_list()
+    print("-------------------------------------------------")
+
+    # Remove duplicates
+    print("Original list:")
+    list.print_list()
+
+    list.remove_duplicates()
+    print("\nList after removing duplicates:")
+    list.print_list()
+    print("-------------------------------------------------")
+
+    # Check if list is palindrome
+    list_palindrome = LinkedList()
+    list_palindrome.append(3)
+    list_palindrome.append(5)
+    list_palindrome.append(8)
+    list_palindrome.append(5)
+    list_palindrome.append(3)
+
+    print("\nIs the list a Palindrome? " + ("Yes" if list_palindrome.is_palindrome() else "No"))
+    print("-------------------------------------------------")
+
+    # find the first intersection node
+    firstlist = Node(1)
+    firstlist.next = Node(2)
+    firstlist.next.next = Node(3)
+    
+    secondlist = Node(4)
+    secondlist.next = firstlist.next.next
+
+    interaction_node = LinkedList()
+    intersection = interaction_node.first_intersection(firstlist, secondlist)
+    
+    
+    if intersection:
+        print(f"The first intersection node data is: {intersection.data}")
+    else:
+        print("No intersection found")
+    print("-------------------------------------------------")
+
+    # find the loop node
+    loop_node = Node(1)
+    loop_node.next = Node(2)
+    loop_node.next.next = Node(3)
+    loop_node.next.next.next = Node(4)
+    loop_node.next.next.next.next = Node(5)
+    loop_node.next.next.next.next = loop_node.next.next
+
+    loop = LinkedList()
+    loop_start = loop.detect_loop(loop_node)
+
+    if loop_start:
+        print(f"The loop starts at node with data: {loop_start.data}")
+    else:
+        print("No loop detected") 
+
+
+
+    
+    
+

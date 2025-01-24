1c52b3dd97cbc9f4126c067c02399d73f885e0db
MbatuEr <eryilmazmehmetbatuhan@gmail.com>
2025-01-24 12:13:01 +0300

    added experiment with Binary Trees.

--- BinaryTrees/BinaryTrees.py
+++ BinaryTrees/BinaryTrees.py
@@ -1,9 +1,19 @@
+import sys
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class TreeNode:
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
         self.parent = None
+        self.level_next_right = None
+        self.locked = False
+        self.locked_descendant_count = 0
 
 class BinaryTree:
     def __init__(self):
@@ -26,12 +36,14 @@ class BinaryTree:
                 return self.insertHelper(node.left, key)
             else:
                 node.left = TreeNode(key)
+                node.left.parent = node
                 return True
         else:
             if node.right:
                 return self.insertHelper(node.right, key)
             else:
                 node.right = TreeNode(key)
+                node.right.parent = node
                 return True
     
     # Looks up for the specified node in the BST.
@@ -101,7 +113,7 @@ class BinaryTree:
     
     # Finds the lowest common ancestor.
     def findLCA(self, root, p, q):
-        if not root or root == p or root == q:
+        if not root or root.val == p or root.val == q:
             return root
         
         left = self.findLCA(root.left, p, q)
@@ -185,3 +197,267 @@ class BinaryTree:
                 nodestack.append(current.left)
         
         return result
+
+    # Helper function to find a node by value.
+    def findValue(self, value):
+        current = self.root
+        while current and current.val != value:
+            if value < current.val:
+                current = current.left
+            else:
+                current = current.right
+        
+        return current
+
+    # Computes the successor of the node in an inorder traversal.
+    def computeSuccessor(self, node):
+        if not node:
+            return None
+        
+        if node.right:
+            current = node.right
+            while current.left:
+                current = current.left
+            
+            return current
+
+        current = node
+        parent = node.parent
+
+        while parent and parent.right == current:
+            current = parent
+            parent = parent.parent
+        
+        return parent
+    
+    # Does an inorder traversal with a O(1) space complexity.
+    def inorderTraversalWithO1Space(self, root):
+        if root is None:
+            return
+        
+        current = self.root
+        prev = None
+
+        while current:
+            if current.parent == prev:
+                if current.left:
+                    prev = current
+                    current = current.left
+                else:
+                    print(current.val, end=" ")
+                    prev = current
+                    current = current.right if current.right else current.parent
+            elif prev == current.left:
+                print(current.val, end=" ")
+                prev = current
+                current = current.right if current.right else current.parent
+            else:
+                prev = current
+                current = current.parent
+
+    # Main function to build a binary tree.
+    def buildTree(self, preorder, inorder):
+        in_map = {val: idx for idx, val in enumerate(inorder)}
+
+        # Helper function to build a binary tree.
+        def buildTreeHelper(pre_start, pre_end, in_start, in_end):
+            if pre_start > pre_end or in_start > in_end:
+                return None
+
+            root_val = preorder[pre_start]
+            root = TreeNode(root_val)
+
+            root_index = in_map[root_val]
+            left_subtree_size = root_index - in_start
+
+            root.left = buildTreeHelper(pre_start + 1, pre_start + left_subtree_size, in_start, root_index - 1)
+            root.right = buildTreeHelper(pre_start + left_subtree_size + 1, pre_end, root_index + 1, in_end)
+
+            return root
+
+        return buildTreeHelper(0, len(preorder) - 1, 0, len(inorder) - 1)
+    
+    # Builds a tree from a preorder traversal and uses null to mark empty children.
+    def reconstructPreorder(self, node):
+        if node is None:
+            return
+
+        nodestack = []
+        nodestack.append(node)
+
+        while nodestack:
+            current = nodestack.pop()
+            print(current.val, end=" ")
+
+            if current.right:
+                nodestack.append(current.right)
+            else:
+                print("NONE", end=" ")
+
+            if current.left:
+                nodestack.append(current.left)
+            else:
+                print("NONE", end=" ")
+    
+    # Helper function to traverse the tree and collect leaves.
+    def collectLeaves(self, root, current):
+        if not root:
+            return current  
+
+        if not root.left and not root.right:
+            current.next = ListNode(root.val)
+            return current.next  
+
+        current = self.collectLeaves(root.left, current)
+        current = self.collectLeaves(root.right, current)
+
+        return current
+
+    # Creates a linked list from leaves of the tree.
+    def createListFromLeaves(self, root):
+        dummy = ListNode(-1)
+        self.collectLeaves(root, dummy)
+        return dummy.next
+
+    # Prints the linked list.
+    def printLinkedList(self, head):
+        while head:
+            print(head.val, end=" ")
+            head = head.next
+    
+    # Computes the exterior of a binary tree.
+    def exteriorOfBinaryTree(self, root):
+        if not root:
+            return
+        
+        current = root
+        while current.left:
+            print(current.val,end=" ")
+            current = current.left
+        
+        leaves = self.createListFromLeaves(root)
+        self.printLinkedList(leaves)
+
+        right_subtree = root
+        nodestack = []
+        nodestack.append(right_subtree)
+
+        while right_subtree.right:
+            right_subtree = right_subtree.right
+            nodestack.append(right_subtree)
+        
+        while nodestack:
+            last_part_of_tree = nodestack.pop()
+            if last_part_of_tree.left or last_part_of_tree.right:
+                print(last_part_of_tree.val,end=" ")
+    
+    # Sets the node to its level next right node.
+    def rightSiblingTree(self, root):
+        if not root or root.left or root.right:
+            return self.setLevelNextField(root.left, root.right)
+        
+    # Helper funtion to set the nodes to next level field.
+    def setLevelNextField(self, left_tree, right_tree):
+        if not left_tree or not right_tree:
+            return
+        left_tree.level_next_right = right_tree
+
+        self.setLevelNextField(left_tree.left, left_tree.right)
+        self.setLevelNextField(right_tree.left, right_tree.right)
+        self.setLevelNextField(left_tree.right, right_tree.left)
+    
+    # Prints the nodes in order of their level.
+    def printLevelNext(self, root):
+        if not root : return
+
+        current = root
+        while current:
+            temp = current
+            print(temp.val, end=" ")
+
+            while temp:
+                if temp.level_next_right:
+                    print(f"-> {temp.level_next_right.val}", end=" ")
+                else:
+                    print("-> NONE")
+            
+                temp = temp.level_next_right
+
+            if current.left:
+                current = current.left
+            elif current.right:
+                current = current.right
+            else:
+                current = None
+
+    # Checks if any ancestors or descendants are locked.
+    def canLockOrUnlock(self, node):
+        if not node: return False
+        ancestor = node.parent
+        while ancestor:
+            if ancestor.locked: return False
+            ancestor = ancestor.parent
+        
+        if node.locked_descendant_count > 0: return False
+
+        return True
+    
+    # Function to lock the node.
+    def lock(self, node):
+        if self.canLockOrUnlock(node):
+            node.locked = True
+            ancestor = node.parent
+
+            while ancestor:
+                ancestor.locked_descendant_count += 1
+                ancestor = ancestor.parent
+            return True
+        
+        return False
+
+    # Function to unlock the node.
+    def unlock(self, node):
+        if self.canLockOrUnlock(node):
+            node.locked = False
+            ancestor = node.parent
+
+            while ancestor:
+                ancestor.locked_descendant_count += 1
+                ancestor = ancestor.parent
+            return True
+        
+        return False
+    
+    # Checks if a binary tree satisfies the BST property.
+    def isBSTHelper(self, node, min_value, max_value):
+        if not node:
+            return True
+
+        if node.val <= min_value or node.val >= max_value:
+            return False
+
+        return (self.isBSTHelper(node.left, min_value, node.val) and
+                self.isBSTHelper(node.right, node.val, max_value))
+
+    # Wrapper function for isBSTHelper().
+    def is_BST(self):
+        return self.isBSTHelper(self.root, float('-inf'), float('inf'))
+
+    # Finds the first key greater than the input key.
+    def firstKeyGreaterHelper(self, node, input_value, first_key_appeared):
+        if not node:
+            return first_key_appeared
+
+        if node.val > input_value:
+            first_key_appeared = min(first_key_appeared, node.val)
+
+        if node.val > input_value:
+            return self.firstKeyGreaterHelper(node.left, input_value, first_key_appeared)
+        else:
+            return self.firstKeyGreaterHelper(node.right, input_value, first_key_appeared)
+
+    # Wrapper function for firstKeyGreaterHelper().
+    def firstKeyGreater(self, input_value):
+        result = self.firstKeyGreaterHelper(self.root, input_value, float('inf'))
+        return -1 if result == float('inf') else result
+          
--- BinaryTrees/main.py
+++ BinaryTrees/main.py
@@ -35,27 +35,19 @@ if __name__ == "__main__":
     print("----------------------------------------------------------")
 
     # Find the lowest common ancestor.
-    rootLCA = TreeNode(8)
-    rootLCA.left = TreeNode(4)
-    rootLCA.right = TreeNode(12)
-    rootLCA.left.left = TreeNode(2)
-    rootLCA.left.right = TreeNode(6)
-    rootLCA.right.left = TreeNode(10)
-    rootLCA.right.right = TreeNode(14)
-    rootLCA.left.left.right = TreeNode(3)
-    rootLCA.right.left.left = TreeNode(9)
-    rootLCA.right.left.right = TreeNode(11)
-    rootLCA.right.right.left = TreeNode(13)
-    rootLCA.right.right.right = TreeNode(16)
+    tree_LCA = BinaryTree()
+    order_of_tree = [8,4,12,2,6,10,14,3,9,11,13,16]
+    for i in order_of_tree:
+        tree_LCA.insert(i)
 
-    p = rootLCA.left.left.right
-    q = rootLCA.left.right
+    p = 3
+    q = 6
 
-    lca = tree.findLCA(rootLCA, p, q)
+    lca = tree_LCA.findLCA(tree_LCA.root, p, q)
     if lca:
-        print(f"The lowest common ancestor of {p.val} and {q.val} is {lca.val}")
+        print(f"The lowest common ancestor of {p} and {q} is {lca.val}")
     else:
-        print(f"The lowest common ancestor of {p.val} and {q.val} is not found.")
+        print(f"The lowest common ancestor of {p} and {q} is not found.")
     print("----------------------------------------------------------")
 
     # Sum of binary numbers represented by root-to-leaf paths
@@ -81,19 +73,16 @@ if __name__ == "__main__":
     print("----------------------------------------------------------")
 
     # Path weight equals a target value
-    rootsumweight = TreeNode(5)
-    rootsumweight.left = TreeNode(3)
-    rootsumweight.right = TreeNode(8)
-    rootsumweight.left.left = TreeNode(2)
-    rootsumweight.left.right = TreeNode(4)
-    rootsumweight.right.left = TreeNode(7)
-    rootsumweight.right.right = TreeNode(11)
-    rootsumweight.left.left.left = TreeNode(1)
-    rootsumweight.right.right.right = TreeNode(13)
+    path_weight = BinaryTree()
+    path_weight_order = [5,3,8,2,4,7,11,1,13]
+
+    for i in path_weight_order:
+        path_weight.insert(i)
 
     target_weight = 20
     current_value = 0
-    if tree.hasPathWeight(rootsumweight, target_weight, current_value):
+
+    if tree.hasPathWeight(path_weight.root, target_weight, current_value):
         print(f"There exists a leaf whose path weight equals {target_weight}.")
     else:
         print(f"No leaf path weight equals {target_weight}.")
@@ -117,3 +106,106 @@ if __name__ == "__main__":
     print("The preorder traversal of the binary tree:", preorder_traversal)
     print("----------------------------------------------------------")
     
+    # Successor of a root.
+    node = tree_order.findValue(10)
+    if node:
+        print(f"node found: {node.val}")
+        successor = tree_order.computeSuccessor(node)
+
+        if successor:
+            print(f"Successor of {node.val} is: {successor.val}")
+        else:
+            print(f"No successor for {node.val}")
+    else:
+        print("Node is not found in the tree.")
+    print("----------------------------------------------------------")
+
+    # Inorder travelsal with O(1) space complexity.
+    print("The inorder traversal of the binary tree: ",end="")
+    tree_order.inorderTraversalWithO1Space(tree_order.root)
+    print("\n----------------------------------------------------------")
+
+    # Build a binary tree.
+    inorder = [4, 5, 6, 7, 8, 9, 10, 12, 13, 14]
+    preorder = [8, 6, 4, 5, 7, 10, 9, 13, 12, 14]
+
+    root_order = tree.buildTree(preorder, inorder)
+    k = 3
+    print("Inorder traversal of the reconstructed tree:")
+    inorder_traversal_tree = tree.inorderTraversal(root_order, k)
+    print(inorder_traversal_tree)
+
+    print("Preorder traversal of the reconstructed tree:")
+    preorder_traversal_tree = tree.preorderTraversal(root_order)
+    print(preorder_traversal_tree)
+    print("\n----------------------------------------------------------")
+
+    # Reconstruct preorder traversal.
+    tree_order.reconstructPreorder(tree_order.root)
+    print("\n----------------------------------------------------------")
+
+    # Create list from leaves.
+    leaflist = tree_order.createListFromLeaves(tree_order.root)
+    print("Linked list of leaves: ", end=" ")
+    tree_order.printLinkedList(leaflist)
+    print("\n----------------------------------------------------------")
+    
+    # Compute exterior. 
+    exterior = BinaryTree()
+    root_exterior = [8,4,13,2,5,11,16,1,3,7,12,14,18,6]
+    for i in root_exterior:
+        exterior.insert(i)
+    exterior.exteriorOfBinaryTree(exterior.root)
+    print("\n----------------------------------------------------------")
+
+    # Right sibling tree.
+    sibling = BinaryTree()
+    right_sibling = [8,4,13,2,6,10,16,1,3,5,7,9,11,14,18]
+    for values in right_sibling:
+        sibling.insert(values)
+    
+    sibling.rightSiblingTree(sibling.root)
+    sibling.printLevelNext(sibling.root)
+    print("----------------------------------------------------------")
+
+    # Lock the node.
+    locked = BinaryTree()
+    root_lock = [4,2,6,1,3,5,7]
+
+    for values in root_lock:
+        locked.insert(values)
+    
+    lock_node1 = locked.findValue(1)
+    lock_node3 = locked.findValue(3)
+    lock_node2 = locked.findValue(2)
+
+    print(f"Locking node 1: {'Success' if locked.lock(lock_node1) else 'Failed'}")
+    print(f"Locking node 3: {'Success' if locked.lock(lock_node3) else 'Failed'}")
+    print(f"Locking node 2 (parent of 1 and 3): {'Success' if locked.lock(lock_node2) else 'Failed'}")
+    print(f"Unlocking node 3: {'Success' if locked.unlock(lock_node3) else 'Failed'}")
+    print(f"Locking node 2 after unlocking node 3: {'Success' if locked.lock(lock_node2) else 'Failed'}")
+    print(f"Unlocking node 1: {'Success' if locked.unlock(lock_node1) else 'Failed'}")
+    print(f"Locking node 2 after unlocking node 1: {'Success' if locked.lock(lock_node2) else 'Failed'}")
+    print("----------------------------------------------------------")
+
+    # Check if the tree satisfies the BST property
+    bst = BinaryTree()
+    root_bst = [8, 4, 12, 2, 6, 10, 14]
+
+    for value in root_bst:
+        bst.insert(value)
+
+    if bst.is_BST():
+        print("The tree satisfies the BST property.")
+    else:
+        print("The tree does NOT satisfy the BST property.")
+
+    # Find the first key greater than the input key
+    input_key = 13
+    first_key = bst.firstKeyGreater(input_key)
+
+    if first_key != -1:
+        print(f"The first key greater than {input_key} is {first_key}.")
+    else:
+        print(f"No key greater than {input_key} found.")
+    print("----------------------------------------------------------")

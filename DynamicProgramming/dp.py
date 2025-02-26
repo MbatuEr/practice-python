85c20f622e9942a9fb0284ce72bdbc171a176681
MbatuEr <eryilmazmehmetbatuhan@gmail.com>
2025-02-26 21:34:01 +0300

    Formatting and comments.

--- DynamicProgramming/dp.py
+++ DynamicProgramming/dp.py
@@ -1,5 +1,4 @@
-from typing import List
-from typing import Dict
+from typing import List, Dict
 
 class Dp:
     def __init__(self):
@@ -10,86 +9,80 @@ class Dp:
             return n
         if n in self.memo:
             return self.memo[n]
-
+        
         self.memo[n] = (
             self.climbing_stairs_top_down(n - 1) +
             self.climbing_stairs_top_down(n - 2)
         )
-
         return self.memo[n]
-
+    
     @staticmethod
     def climbing_stairs_bottom_up(n: int) -> int:
         if n <= 2:
             return n
-
-        one_step_before , two_step_before = 2, 1
-
+        one_step_before, two_step_before = 2, 1
         for i in range(3, n + 1):
             current = one_step_before + two_step_before
             two_step_before = one_step_before
             one_step_before = current
-
+        
         return current
+    
+    @staticmethod
+    def min_coin_combination_top_down(coins: List[int], target: int) -> int:
+        def top_down_up_dp(coins: List[int], target: int, memo: Dict[int, int]) -> int:
+            if target == 0:
+                return 0
+            if target in memo:
+                return memo[target]
+            min_coins = float("inf")
+            for coin in coins:
+                if coin <= target:
+                    min_coins = min(min_coins, 1 + top_down_up_dp(coins, target - coin, memo))
 
-    def min_coin_combination_top_down(self, coins: List[int], target: int) -> int:
-        res = self.top_down_up_dp(coins, target, {})
-        return -1 if res == float("inf") else res
-
-    def top_down_up_dp(self, coins: List[int], target: int, memo: Dict[int, int]) -> int:
-        if target == 0:
-            return 0
-        if target in memo:
+            memo[target] = min_coins
             return memo[target]
-        min_coins = float("inf")
-        for coin in coins:
-            if coin <= target:
-                min_coins = min(min_coins, 1 + self.top_down_up_dp(coins, target - coin, memo))
-
-        memo[target] = min_coins
-        return memo[target]
-
+        res = top_down_up_dp(coins, target, {})
+        return -1 if res == float("inf") else res
+    
     @staticmethod
     def min_coin_combination_bottom_up(coins: List[int], target: int) -> int:
         dp = [float("inf")] * (target + 1)
         dp[0] = 0
-
         for t in range(1, target + 1):
             for coin in coins:
                 if coin <= t:
                     dp[t] = min(dp[t], 1 + dp[t - coin])
+        
         return dp[target] if dp[target] != float("inf") else -1
-
+    
     @staticmethod
     def matrix_pathways(m: int, n: int) -> int:
         prev_row = [1] * n
-
         for row in range(1, m):
             current_row = [1] * n
             for col in range(1, n):
                 current_row[col] = current_row[col - 1] + prev_row[col]
-
             prev_row = current_row
-
+        
         return prev_row[n - 1]
-
+    
     @staticmethod
-    def neighbour_burglary(houses: List[int]) -> int:
+    def neighbor_burglary(houses: List[int]) -> int:
         if not houses:
             return 0
         if len(houses) == 1:
             return houses[0]
-
-        prev_max_profit = max(houses[0], houses[1])
-        prev_prev_max_profit = houses[0]
-
+        
+        prev_max_frofit = max(houses[0], houses[1])
+        prev_prev_max_frofit = houses[0]
         for i in range(2, len(houses)):
-            current_profit = max(prev_max_profit, houses[i] + prev_prev_max_profit)
-            prev_prev_max_profit = prev_max_profit
-            prev_max_profit = current_profit
+            current_profit = max(prev_max_frofit, houses[i] + prev_prev_max_frofit)
+            prev_prev_max_frofit = prev_max_frofit
+            prev_max_frofit = current_profit
 
         return current_profit
-
+    
     @staticmethod
     def longest_common_subsequence(s1: str, s2: str) -> int:
         prev_row = [0] * (len(s2) + 1)
@@ -101,9 +94,10 @@ class Dp:
                 else:
                     current_row[j] = max(prev_row[j], current_row[j + 1])
             prev_row = current_row
+        
         return prev_row[0]
-
-    def longest_palindrome_in_a_string(self, s: str) -> str:
+    
+    def largest_palindrome_in_a_string(self, s: str) -> str:
         n = len(s)
         start, max_len = 0, 0
         for center in range(n):
@@ -124,16 +118,18 @@ class Dp:
             right += 1
 
         return left, right - left + 1
-
+    
     @staticmethod
     def maximum_subarray_sum(nums: List[int]) -> int:
         n = len(nums)
         if n == 0:
             return 0
+        
         current_sum = max_sum = nums[0]
         for i in range(1, n):
             current_sum = max(nums[i], current_sum + nums[i])
             max_sum = max(max_sum, current_sum)
+            
         return max_sum
 
     @staticmethod
@@ -155,6 +151,7 @@ class Dp:
     def largest_square_in_a_matrix(matrix: List[List[int]]) -> int:
         if not matrix:
             return 0
+        
         m, n = len(matrix), len(matrix[0])
         prev_row = [0] * n
         max_len = 0
@@ -167,9 +164,7 @@ class Dp:
                 else:
                     if matrix[i][j] == 1:
                         current_row[j] = 1 + min(current_row[j - 1], prev_row[j - 1], prev_row[j])
-
                 max_len = max(max_len, current_row[j])
-
             prev_row, current_row = current_row, [0] * n
 
         return max_len ** 2
--- DynamicProgramming/main.py
+++ DynamicProgramming/main.py
@@ -3,55 +3,54 @@ from dp import Dp
 if __name__ == "__main__":
     dp = Dp()
     # Climbing stairs
-    a = 4
-    b = dp.climbing_stairs_top_down(a)
-    print("Result: ", b)
+    stairs_length = 4
+    min_step_top_down = dp.climbing_stairs_top_down(stairs_length)
+    print("Min step:", min_step_top_down)
     print("-" * 60)
     
-    bu = 5
-    ub = dp.climbing_stairs_top_down(bu)
-    print("Result: ", ub)
+    min_step_bottom_up = dp.climbing_stairs_bottom_up(stairs_length)
+    print("Min step:", min_step_bottom_up)
     print("-" * 60)
     
     # Coin combination
     input_coins = [1, 2, 3, 4, 5, 6] 
     target = 13
-    res = dp.min_coin_combination_top_down(input_coins, target)
-    print("Result: ", res)
+    coin_combination = dp.min_coin_combination_top_down(input_coins, target)
+    print("The number of coins to reach the target value:", coin_combination)
     print("-" * 60)
     
-    res_bu = dp.min_coin_combination_bottom_up(input_coins, target)
-    print("Result: ", res_bu)
+    coin_bottom_up = dp.min_coin_combination_bottom_up(input_coins, target)
+    print("The number of coins to reach the target value:", coin_bottom_up)
     print("-" * 60)
     
     # Matrix pathways
     m, n = 3, 3
     matrix = dp.matrix_pathways(m, n)
-    print("Result: ", matrix)
+    print("Number of pathways to complete the matrix::", matrix)
     print("-" * 60)
     
     # Max profit
     Input_houses = [200, 300, 200, 50, 250, 300]
-    max_gain = dp.neighbour_burglary(Input_houses)
-    print("Result: ", max_gain)
+    max_gain = dp.neighbor_burglary(Input_houses)
+    print("Max amount of money can be stolen:", max_gain)
     print("-" * 60)
     
     # Most common characters
     s1, s2 = "acabac", "aebab"
     lcs = dp.longest_common_subsequence(s1, s2)
-    print("Result: ", lcs)
+    print("Number of common characters:", lcs)
     print("-" * 60)
     
     # Largest palindrome
     Input_s = "abccbababc"
-    output_s = dp.longest_palindrome_in_a_string(Input_s)
-    print("Result: ", output_s)
+    output_s = dp.largest_palindrome_in_a_string(Input_s)
+    print("Largest palindrome in the string:", output_s)
     print("-" * 60)
     
     # Max sum of a subarray
     Input_nums = [3, 1, -6, 2, -1, 4, -9]
     output_num = dp.maximum_subarray_sum(Input_nums)
-    print("Result: ", output_num)
+    print("Max sum of a subarray:", output_num)
     print("-" * 60)
     
     # Max value with capacity
@@ -59,7 +58,7 @@ if __name__ == "__main__":
     weights = [5, 3, 4, 1] 
     values = [70, 50, 40, 10]
     best_knapstack = dp.knapsack(cap, weights, values)
-    print("Result: ", best_knapstack)
+    print("The best combination of goods:", best_knapstack)
     print("-" * 60)
     
     # Largest square
@@ -71,6 +70,6 @@ if __name__ == "__main__":
         [1, 1, 1, 0, 1]
     ]
     largest_square = dp.largest_square_in_a_matrix(square_matrix)
-    print("Result: ", largest_square)
+    print("Area of largest square with value 1:", largest_square)
     print("-" * 60)
     
